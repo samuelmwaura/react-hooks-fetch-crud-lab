88045624ed9a5b6008fab9c48dfbdf7f5952c9bb
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-07-24 22:33:18 +0300

    Completed the lab.

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,44 @@
-import React, { useState } from "react";
+import React, { useState,useEffect } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
 function App() {
   const [page, setPage] = useState("List");
+  const [questions,setQuestions]=useState([])
+
+useEffect(()=>{
+fetch('http://localhost:4000/questions')
+.then(response=>response.json())
+.then(data=> {
+  console.log(data);
+  setQuestions(data)})
+.catch(error=>console.log(error))
+},[]);
+
+
+function handleNewQuestionAdd(newQuestion){
+  setQuestions([...questions,newQuestion])
+  console.log(newQuestion);
+}
+
+function deleteItem(deletedQuestion){
+setQuestions(()=>questions.filter(question=>question.id !== deletedQuestion.id))
+}
+
+function updateAnswer(updatedAnswer){
+setQuestions(questions.map(question=>{
+  if(question.id === updatedAnswer.id) return updatedAnswer
+ return question;
+}));
+}
+
+
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
-      {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {page === "Form" ? <QuestionForm handleNewQuestionAdd={handleNewQuestionAdd}/> : <QuestionList questions={questions} deleteItem={deleteItem} updateAnswer={updateAnswer}/>}
     </main>
   );
 }
--- src/components/QuestionList.js
+++ src/components/QuestionList.js
@@ -1,10 +1,13 @@
 import React from "react";
+import QuestionItem from "./QuestionItem";
 
-function QuestionList() {
+function QuestionList({questions,deleteItem,updateAnswer}) {
   return (
     <section>
       <h1>Quiz Questions</h1>
-      <ul>{/* display QuestionItem components here after fetching */}</ul>
+      <ul>{/* display QuestionItem components here after fetching */ questions.map(question=>{
+        return <QuestionItem key={question.id} question={question} deleteItem={deleteItem} updateAnswer={updateAnswer}/>
+      })}</ul>
     </section>
   );
 }
