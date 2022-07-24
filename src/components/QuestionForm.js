bc8936fe0c5d915efcf97ff688de98d8aadac5c0
Samuel Mwaura <samuelmwaura081@gmail.com>
2022-07-24 22:32:05 +0300

    commit tree 2ba4761b61917c8395539095e75c6a5172b6eb2c

--- src/components/QuestionForm.js
+++ src/components/QuestionForm.js
@@ -20,6 +20,21 @@ function QuestionForm(props) {
   function handleSubmit(event) {
     event.preventDefault();
     console.log(formData);
+    fetch('http://localhost:4000/questions',{
+      method:'POST',
+      headers:{
+        'Content-Type':'Application/json',
+         'Accept':'Application/json'
+      },
+      body:JSON.stringify({
+        prompt:formData.prompt,
+        answers:[formData.answer1,formData.answer2,formData.answer3,formData.answer4],
+        correctIndex:formData.correctIndex
+      })
+    })
+    .then(response=>response.json())
+    .then(data=>props.handleNewQuestionAdd(data))
+    .catch(error=>console.log(error))
   }
 
   return (
--- src/components/QuestionItem.js
+++ src/components/QuestionItem.js
@@ -1,7 +1,7 @@
 import React from "react";
 
-function QuestionItem({ question }) {
-  const { id, prompt, answers, correctIndex } = question;
+function QuestionItem({ question,deleteItem,updateAnswer }) {
+  const { id, prompt,answers,correctIndex } = question;
 
   const options = answers.map((answer, index) => (
     <option key={index} value={index}>
@@ -9,15 +9,43 @@ function QuestionItem({ question }) {
     </option>
   ));
 
+
+  function handleQuestionDelete(){
+  
+    fetch(`http://localhost:4000/questions/${question.id}`,{
+      method:'DELETE'
+    })
+    .then(response=>response.json())
+    .then(data=>deleteItem(question))
+    .catch(error=>console.log(error));
+
+  }
+
+  function handleChoiceChange(event){
+    fetch(`http://localhost:4000/questions/${question.id}`,{
+      method:'PATCH',
+      headers:{
+        'Content-Type':'Application/json',
+        "Accept":"Application/json"
+      },
+      body:JSON.stringify({...question,correctIndex:event.target.value})
+    })
+    .then(response=>response.json())
+    .then(data=>updateAnswer(data))
+    .catch(error=>console.log(error))
+  }
+
+
+  //there is the state that is responsible for displaying all the questions, that is the one that should just be updated and not creating another one here.
   return (
     <li>
       <h4>Question {id}</h4>
       <h5>Prompt: {prompt}</h5>
       <label>
         Correct Answer:
-        <select defaultValue={correctIndex}>{options}</select>
+        <select defaultValue={correctIndex} onChange={handleChoiceChange}>{options}</select>
       </label>
-      <button>Delete Question</button>
+      <button onClick={handleQuestionDelete}>Delete Question</button>
     </li>
   );
 }
